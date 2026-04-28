bb4af0f1a722eb39a1e2e424ca70ae8171a4a0ab
Huseyn Hasanli <huseynhesenli2005@gmail.com>
2026-04-28 18:38:01 +0400

    Add terraform base files

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,1 +1,4 @@
-
+resource "azurerm_resource_group" "main" {
+  name     = "rg-${local.suffix}"
+  location = var.location
+}
---
+++ infra/terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+  default = "project2"
+}
+
+variable "group" {
+  default = "g6"
+}
+
+variable "location" {
+  default = "uaenorth"
+}
+
+locals {
+  suffix = "${var.project}-${var.group}"
+}
