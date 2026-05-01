e755ec4049e50611f8a6bde39443a4ede05ebaec
Huseyn Hasanli <huseynhesenli2005@gmail.com>
2026-05-01 10:44:22 +0400

    add ansible inventory

--- infra/terraform/modules/keyvault/outputs.tf
+++ infra/terraform/modules/keyvault/outputs.tf
@@ -1,7 +1,15 @@
-output "key_vault_id" {}
+output "key_vault_id" {
+  value = azurerm_key_vault.main.id
+}
 
-output "key_vault_name" {}
+output "key_vault_name" {
+  value = azurerm_key_vault.main.name
+}
 
-output "key_vault_uri" {}
+output "key_vault_uri" {
+  value = azurerm_key_vault.main.vault_uri
+}
 
-output "private_endpoint_ip" {}
+output "private_endpoint_ip" {
+  value = try(azurerm_private_endpoint.keyvault.private_service_connection[0].private_ip_address, null)
+}
--- infra/terraform/modules/keyvault/variables.tf
+++ infra/terraform/modules/keyvault/variables.tf
@@ -1,21 +1,54 @@
-variable "name" {}
+variable "name" {
+  type        = string
+  description = "Globally unique Key Vault name."
+}
 
-variable "resource_group_name" {}
+variable "resource_group_name" {
+  type        = string
+  description = "Resource group name."
+}
 
-variable "location" {}
+variable "location" {
+  type        = string
+  description = "Azure region."
+}
 
-variable "tenant_id" {}
+variable "tenant_id" {
+  type        = string
+  description = "Microsoft Entra tenant ID."
+}
 
-variable "virtual_network_id" {}
+variable "virtual_network_id" {
+  type        = string
+  description = "VNet linked to privatelink.vaultcore.azure.net."
+}
 
-variable "ops_subnet_id" {}
+variable "ops_subnet_id" {
+  type        = string
+  description = "Subnet for the ops runner and Key Vault private endpoint."
+}
 
-variable "ops_runner_principal_id" {}
+variable "ops_runner_principal_id" {
+  type        = string
+  description = "Managed identity principal ID of the ops runner VM."
+}
 
-variable "sku_name" {}
+variable "sku_name" {
+  type    = string
+  default = "standard"
+}
 
-variable "purge_protection_enabled" {}
+variable "purge_protection_enabled" {
+  type    = bool
+  default = true
+}
 
-variable "soft_delete_retention_days" {}
+variable "soft_delete_retention_days" {
+  type    = number
+  default = 7
+}
 
-variable "tags" {}
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
